561803486a7c959c259e452144edc8bf2ba7a768
killshotxd <hassanansari211@gmail.com>
2022-09-01 19:33:33 +0530

    Form Design Complete

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "./Account.module.css";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../Firebase";
+import { Select, Textarea } from "@chakra-ui/react";
 import { signOut } from "firebase/auth";
 import { Button, FormLabel, Input } from "@chakra-ui/react";
 
@@ -45,6 +46,33 @@ const Account = (props) => {
           <div className={styles.title}>
             <FormLabel>Title</FormLabel>
             <Input variant="filled" placeholder="Title for your Quote" />
+          </div>
+          <div className={styles.title}>
+            <FormLabel>Written By</FormLabel>
+            <Input variant="filled" placeholder="Name..." />
+          </div>
+          <div className={styles.title}>
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              variant="filled"
+              placeholder="Enter Your Quote.."
+            ></Textarea>
+          </div>
+          <div className={styles.title}>
+            <FormLabel>Categories</FormLabel>
+            <Select
+              variant="filled"
+              className={styles.select}
+              placeholder="Select categories"
+            >
+              <option value="option1">Happy</option>
+              <option value="option2">Sad</option>
+              <option value="option3">Funny</option>
+            </Select>
+          </div>
+          <div className={styles.btn}>
+            <Button colorScheme="pink">Add</Button>
+            <Button colorScheme="pink">Cancel</Button>
           </div>
         </div>
       </div>
